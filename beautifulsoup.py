5459f02329007e9316856c620b3c80836f121286
Sadiq <dksadiq@g.com>
2020-04-27 23:33:28 +0300

    Updated

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -5,6 +5,15 @@ whitehouse_url = "https://www.whitehouse.gov/briefings-statements/"
 whitehouse_page = requests.get(whitehouse_url)
 whitehouse_src = whitehouse_page.content
 
-wh_soup = BeautifulSoup(whitehouse_src, "lxml")
+wh_soup = BeautifulSoup(whitehouse_src, features="lxml")
 links = wh_soup.find_all("a")
-print(links)
+h2_links = wh_soup.find_all("h2")
+print(h2_links)
+# print(wh_soup.a.string)
+
+count = 0
+for link in links:
+    if link.find_parent("h2"):
+        count += 1
+        print(str(count) + ". " + link.string)
+        # print("\n")
